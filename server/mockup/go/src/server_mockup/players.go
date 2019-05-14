b5dc2345ac7f26fecf8710b3d8b8fcd6c6c42703
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-14 16:14:39 +0200

    Adding server mockup.

---
+++ server/mockup/go/src/server_mockup/players.go
@@ -0,0 +1,27 @@
+package main
+
+type Direction int
+
+const (
+    Left = 0
+    Right = 1
+    Up = 2
+    Down = 3
+)
+
+type Item int
+
+
+
+type Inventory struct {
+    Items []Item
+}
+
+type Player struct {
+    Direction int
+    Inventory Inventory
+}
+
+func (player Player) Strings() {
+
+}
