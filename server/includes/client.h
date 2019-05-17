88bb6db70a681539de9a51f951698b15d0c6beae
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-17 11:55:02 +0200

    Adding prototype of client commands and functions.

--- server/includes/client.h
+++ server/includes/client.h
@@ -7,6 +7,9 @@
 
 #pragma once
 
+#include <stdlib.h>
+#include "generic_list.h"
+
 /*
 ** Define directions that can have a player.
 */
@@ -16,6 +19,20 @@ enum DIRECTION {
     UP      = 2,
     DOWN    = 3
 };
+
+/*
+** Define a client inventory
+** each of it's field is a props gatherable on the map.
+*/
+typedef struct inventory_s {
+    unsigned int linemate;
+    unsigned int deraumere;
+    unsigned int sibur;
+    unsigned int mendiane;
+    unsigned int phiras;
+    unsigned int thystame;
+    unsigned int food;
+} inventory_t;
 
 /*
 ** Define a position in 2 dimensions.
@@ -39,11 +56,79 @@ typedef struct pos_s {
 */
 typedef struct client_s {
     size_t id;
-    command_queue_t *commands;
+    list_t *commands;
     unsigned int level;
     unsigned int direction;
     pos_t position;
     inventory_t inventory;
 } client_t;
 
-#endif /* CLIENT_H_ */
+client_t *client_create(size_t id);
+
+void print_client(const client_t *client);
+void print_client_list(const void *data);
+
+/*
+** Client commands.
+*/
+
+/*
+** Moving command
+**  response:   ok
+**  time limit: 7/f
+*/
+char *forward(client_t *client);
+char *left(client_t *client);
+char *right(client_t *client);
+
+/*
+** Look command
+**  response:   [player, tile1-content, tile2-content, ...]
+**  time limit: 7/f
+*/
+char *look(client_t *client);
+
+/*
+** Inventory command
+**  response:   [linemate X, demeraute Y, ..., phiras Z]
+**      -> where `X` `Y` `Z` number possesed by the player.
+**  time limit: 1/f
+*/
+char *inventory(client_t *client);
+
+/*
+** Broadcast [text] command
+**  response:   ok
+**  time limit: 7/f
+*/
+char *broadcast(client_t *client, const char *text);
+
+/*
+** Fork command
+**  response:   ok
+**  time limit: 42/f
+*/
+char *fork_client();
+
+/*
+** Eject command
+**  response:   ok/ko
+**  time limit: 7/f
+*/
+char *eject();
+
+/*
+** Map interaction command
+**  response:   ok/ko
+**  time limit: 7/f
+*/
+char *take(client_t *client);
+char *set(client_t *client);
+
+/*
+** Incantation command
+**  response:   Elevation underway\nCurrent level: X/ko
+**      -> where X is the client->level
+**  time limit: 300/f
+*/
+char *incante(client_t *client);
