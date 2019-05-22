e931905c19ef706c3ec20164f59da256a1f03390
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-22 16:48:53 +0200

    Setup dispatch.

--- server/sources/zappy.c
+++ server/sources/zappy.c
@@ -8,9 +8,29 @@
 #include "zappy.h"
 #include "logger.h"
 
+static int on_connect(int socket, void *data)
+{
+
+}
+
+static int on_delete(int socket, void *data)
+{
+
+}
+
+static int on_delete(int socket, void *data)
+{
+
+}
+
 int zappy_server(server_t *server)
 {
-    infol("Launching Zappy server.\n");
-    print_server(server);
+    infol("Launching `Zappy` runtime server.\n");
+    while (42) {
+        if (dispatch() == -1) {
+            infol("Quitting server after an error.\n");
+            return 84;
+        }
+    }
     return 0;
 }
