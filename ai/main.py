c7e0cd5a97e7eec5335764fcf6d4592ae9e9a58a
Michel Ange SHERFAN <michel-ange.sherfan@epitech.eu>
2019-05-23 17:29:19 +0200

    :sparkles: Added new features in look around command.

--- ai/main.py
+++ ai/main.py
@@ -11,81 +11,6 @@ from ai.parse_inventory import parse_inventory
 import socket
 import sys
 
-class vision:
-    """Structure to stock Inventory"""
-    def __init__(self, tile_nb, food, linemate, deraumere, siur, mendiane, phiras, thystame):
-        self.tile_nb = tile_nb
-        self.food = food
-        self.linemate = linemate
-        self.deraumere = deraumere
-        self.siur = siur
-        self.mendiane = mendiane
-        self.phiras = phiras
-        self.thystame = thystame
-
-    def __str__(self):
-        ret = "Vision -> tile_nb: {} food: {} linemate: {} deraumere: {} siur: {} mendiane: {} phiras: {} thystame: {}>".format(
-            self.tile_nb,
-            self.food,
-            self.linemate,
-            self.deraumere,
-            self.siur,
-            self.mendiane,
-            self.phiras,
-            self.thystame
-        )
-        return ret
-
-    def __repr__(self):
-        return str(self)
-
-def parse_vision(line):
-    """Look arround in return array with present object"""
-    print("In parse Vison\n")
-    environnement = []
-    line = line.replace("[ ", "")
-    line = line.replace(" ]", "")
-    # print("line -> ", line)
-    array = str(line).split(",")
-    # print("sss -> ", array)
-
-    tile = 0
-    food = 0
-    linemate = 0
-    deraumere = 0
-    siur = 0
-    mendiane = 0
-    phiras = 0
-    thystame = 0
-    for x in array:
-        splited = x.split(" ")
-        # print("splited -> ", splited)
-        food = 0
-        linemate = 0
-        deraumere = 0
-        siur = 0
-        mendiane = 0
-        phiras = 0
-        thystame = 0
-        for i in splited:
-            if (str(i) == "food"):
-                food = food + 1
-            if (str(i) == "linemate"):
-                linemate = linemate + 1
-            if (str(i) == "deraumere"):
-                deraumere = deraumere + 1
-            if (str(i) == "siur"):
-                siur = siur + 1
-            if (str(i) == "mendiane"):
-                mendiane = mendiane + 1
-            if (str(i) == "phiras"):
-                phiras = phiras + 1
-            if (str(i) == "thystame"):
-                thystame = thystame + 1
-        tile = tile + 1
-        environnement.append(vision(tile, food, linemate, deraumere, siur, mendiane, phiras, thystame))
-    for x in environnement:
-        print(x)
 
 def test(server_socket):
     print("In test\n")
@@ -139,4 +64,5 @@ def main() -> None:
     client_info = get_client_nb_and_world_size(server_socket, opt)
     print("Client info -> ", repr(client_info) + "\n")
 
+    # while (1):
     test(server_socket)
---
+++ ai/parse_vision.py
@@ -0,0 +1,68 @@
+"""Parse the Look command -> return Array of Object with present things."""
+
+
+class vision:
+    """Structure to stock Inventory"""
+    def __init__(self, tile_nb, food, linemate, deraumere, siur, mendiane, phiras, thystame):
+        """__init__."""
+        self.tile_nb = tile_nb
+        self.food = food
+        self.linemate = linemate
+        self.deraumere = deraumere
+        self.siur = siur
+        self.mendiane = mendiane
+        self.phiras = phiras
+        self.thystame = thystame
+
+    def __str__(self):
+        """__str__."""
+        ret = "Vision -> tile_nb: {} food: {} linemate: {} deraumere: {} siur: {} mendiane: {} phiras: {} thystame: {}>".format(
+            self.tile_nb,
+            self.food,
+            self.linemate,
+            self.deraumere,
+            self.siur,
+            self.mendiane,
+            self.phiras,
+            self.thystame
+        )
+        return ret
+
+    def __repr__(self):
+        """__repr__."""
+        return str(self)
+
+def parse_vision(line):
+    """Look arround in return array with present object"""
+    print("In parse Vison\n")
+    environnement = []
+    line = line.replace("[ ", "")
+    line = line.replace(" ]", "")
+    # print("line -> ", line)
+    array = str(line).split(",")
+    # print("sss -> ", array)
+
+    food = linemate = deraumere = siur = mendiane = phiras = thystame = 0
+    for x in array:
+        splited = x.split(" ")
+        # print("splited -> ", splited)
+        food = linemate = deraumere = siur = mendiane = phiras = thystame = 0
+        for i in splited:
+            if (str(i) == "food"):
+                food = food + 1
+            elif (str(i) == "linemate"):
+                linemate = linemate + 1
+            elif (str(i) == "deraumere"):
+                deraumere = deraumere + 1
+            elif (str(i) == "siur"):
+                siur = siur + 1
+            elif (str(i) == "mendiane"):
+                mendiane = mendiane + 1
+            elif (str(i) == "phiras"):
+                phiras = phiras + 1
+            elif (str(i) == "thystame"):
+                thystame = thystame + 1
+        tile = tile + 1
+        environnement.append(vision(tile, food, linemate, deraumere, siur, mendiane, phiras, thystame))
+    for x in environnement: # print current vision
+        print(x)
