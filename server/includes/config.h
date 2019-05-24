a20da3121d780f57144da6af3012a4a16d66bcee
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-24 10:38:36 +0200

    Adding config.h constant value file.

---
+++ server/includes/config.h
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2019
+** config.h
+** File description:
+** Config global const and other things.
+*/
+
+#pragma once
+
+// Version of the program.
+static const char *VERSION = "0.2.0"
+
+// Default value for `-f` argument.
+static unsigned int DEFAULT_FREQ = 100;
+
+// Default value for life unit at the beginning of the runtime.
+static unsigned int DEFAULT_LIFE_UNIT = 10;
