3e7179e6b6a8c6588af6d06d298b005d29a18ac0
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-24 13:56:58 +0200

    :sparkles: Adding command forward.

--- server/sources/client/client.c
+++ server/sources/client/client.c
@@ -8,6 +8,20 @@
 #include "logger.h"
 #include "client.h"
 
+void move_client(client_t *client, int x, int y, pos_t size)
+{
+    client->position.x += x;
+    client->position.y += y;
+
+    client->position.x %= size.x;
+    client->position.y %= size.y;
+
+    if (client->position.x < 0)
+        client->position.x = (size.x - x) % size.x;
+    if (client->position.y < 0)
+        client->position.y = (size.y - y) % size.y;
+}
+
 client_t *client_create(size_t id)
 {
     client_t *new = calloc(sizeof(client_t), 1);
