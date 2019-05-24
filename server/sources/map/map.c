595cd453dd41f22ba800cb976d201c7f9529d52b
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-24 14:55:08 +0200

    Adding map.

---
+++ server/sources/map/map.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2019
+** map.c
+** File description:
+** Main map file.
+*/
+
+#include "map.h"
+
+map_t *create_map(unsigned int width, unsigned int height)
+{
+    map_t *new = malloc(sizeof(map_t));
+    unsigned int total = width * height;
+
+    new->width = width;
+    new->height = height;
+    new->tiles = malloc(sizeof(tile_t) * total);
+    for (unsigned int i = 0; i < total; ++i)
+         set_tile(&new->tiles[i]);
+    return new;
+}
+
+
+inline tile_t *get_tile_map(map_t *map, unsigned int x, unsigned int y)
+{
+    return &map->tiles[(y * map->width) + x];
+}
