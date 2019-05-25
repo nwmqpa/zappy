272834203db8a6a62a656f2a6f7b1ba6f50eabb3
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-25 12:25:02 +0200

    :art: Moving handlers callback to file and creating a header corresponding.

---
+++ server/sources/server/client_handler.c
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2019
+** client_handler.c
+** File description:
+** Client handler function callback.
+*/
+
+int on_connect_client(int socket, void *data)
+{
+    debugl("Client connect handler.\n");
+    return 0;
+}
+
+int on_delete_client(int socket, void *data)
+{
+    debugl("Client delete handler.\n");
+    return 0;
+}
+
+int on_active_client(int socket, void *data)
+{
+    debugl("Client active handler.\n");
+    return 0;
+}
