848f991e29f29c76e4af2c6d546eb08b58621235
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-26 20:24:47 +0200

    :sparkles: Adding inventory command.

---
+++ server/sources/client/inventory.c
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2019
+** inventory.c
+** File description:
+** Inventory command file.
+*/
+
+#include "client.h"
+
+char *inventory(client_t *client, server_t *server)
+{
+    char *inv = NULL;
+
+    client->cooldown = 1;
+    asprintf(&inv, "[linemate %d, deraumere %d, sibur %d, mendiane %d,"
+            " phiras %d, thystame %d, food %d]",
+            client->inventory.linemate,
+            client->inventory.deraumere,
+            client->inventory.sibur,
+            client->inventory.mendiane,
+            client->inventory.phiras,
+            client->inventory.thystame,
+            client->inventory.food);
+    return inv;
+}
