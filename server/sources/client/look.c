5d34be7ecd5e70e2f4e04fa4f4015e34c1e00ee4
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-26 23:11:59 +0200

    :sparkles: Adding look structure.

---
+++ server/sources/client/look.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** look.c
+** File description:
+** Look around command file.
+*/
+
+#include "client.h"
+
+char *get_vision_content(server_t *server, unsigned int level,
+        unsigned int direction, pos_t position)
+{
+    // TODO: Implement.
+    return "[tile1, tile2, tile3]";
+}
+
+char *look(client_t *client, server_t *server)
+{
+    client->cooldown = 7;
+    return get_vision_content(server, client->level, client->direction,
+            client->position);
+}
