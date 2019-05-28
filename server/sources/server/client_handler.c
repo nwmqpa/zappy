60cf75116dbae5276303d4e0998e1cad05eea43e
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-05-28 18:58:53 +0200

    :sparkles: Adding client connection.

--- server/sources/server/client_handler.c
+++ server/sources/server/client_handler.c
@@ -18,12 +18,25 @@ static int deleting_client_cmp(const void *id_checked, const void *id)
     return lhs == rhs;
 }
 
+void handle_protocol(client_t *client, server_t *server)
+{
+    char team_name[100] = {0};
+    int free_space = -1;
+
+    write(client->id, "WELCOME\n", 8);
+    read(client->id, &team_name, 100);
+    free_space = add_client_to_team(server, client, team_name);
+    dprintf(client->id, "%d\n", free_space);
+}
+
 int on_connect_client(int socket, void *data)
 {
     server_t *server = (server_t *) data;
     client_t *client = client_create(socket);
 
     debugl("Client connect handler.\n");
+    client->id = socket;
+    handle_protocol(client, server);
     insert_head_list(server->clients, (void *) client);
     return 0;
 }
@@ -31,7 +44,7 @@ int on_connect_client(int socket, void *data)
 int on_delete_client(int socket, void *data)
 {
     server_t *server = (server_t *) data;
-    client_t *client = 0;
+    client_t *client = NULL;
 
     debugl("Client delete handler.\n");
     client = pop_cmp_list(
