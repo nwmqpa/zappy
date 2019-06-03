5a33eb4432f8c88a15fb97348ec374c812f0a011
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-03 10:38:17 +0200

    :zap: Adding check for team name.

--- server/sources/server/options.c
+++ server/sources/server/options.c
@@ -17,13 +17,20 @@ nameX       is the name of the team X\n\
 clientsNb   is the number of authorized clients per team\n\
 freq        is the reciprocal of time unit for execution of actions\n";
 
-static int get_names(options_t *options, char *argv[], int argc)
+static inline int count_name(int argc, char *argv[])
 {
     int i = 0;
 
     for (int oldind = optind - 1;
             argc != oldind && argv[oldind][0] != '-'; oldind++)
         i++;
+    return i;
+}
+
+static int get_names(options_t *options, int argc, char *argv[])
+{
+    int i = count_name(argc, argv);
+
     options->name = malloc(sizeof(char *) * (i + 1));
     if (!options->name)
         return -1;
@@ -31,6 +38,11 @@ static int get_names(options_t *options, char *argv[], int argc)
     for (int oldind = optind - 1;
             argc != oldind && argv[oldind][0] != '-'; oldind++) {
         options->name[i] = strdup(argv[oldind]);
+        if (!options->name[i]) {
+            free(options->name);
+            options->name = NULL;
+            return -1;
+        }
         i++;
     }
     options->name[i] = 0;
@@ -51,12 +63,11 @@ options_t *create_opt(int argc, char *argv[])
                 break;
             case 'x':
                 options->width = atoi(optarg);
-                break;
-            case 'y':
+                break; case 'y':
                 options->height = atoi(optarg);
                 break;
             case 'n':
-                get_names(options, argv, argc);
+                get_names(options, argc, argv);
                 break;
             case 'c':
                 options->client_nb = atoi(optarg);
@@ -76,20 +87,20 @@ options_t *create_opt(int argc, char *argv[])
 
 int check_opt(options_t *options)
 {
-    if (options->width > 30) {
-        errorl("Tool long width.");
+    if (options->width > 30 || options->width < 1) {
+        errorl("Invalid width.");
         return -1;
-    }
-    if (options->height > 30) {
-        errorl("Tool long height.");
+    } else if (options->height > 30 || options->height < 1) {
+        errorl("Invalid height.");
         return -1;
-    }
-    if (options->freq <= 0) {
+    } else if (options->freq <= 0) {
         errorl("Invalid frequence.");
         return -1;
-    }
-    if (options->client_nb < 0) {
+    } else if (options->client_nb < 0) {
         errorl("Invalid client_nb.");
+        return -1;
+    } else if (options->name == NULL) {
+        errorl("Invalid team name");
         return -1;
     }
     return 0;
@@ -97,14 +108,14 @@ int check_opt(options_t *options)
 
 void print_opt(options_t *options)
 {
-    debugl("Option {\n"
+    printf("Option {\n"
             "    port: %hd,\n"
             "    width: %d,\n"
             "    height: %d,\n"
             "    name: [", options->port, options->width, options->height);
     for (int i = 0; options->name[i]; i++)
-        debugl("%s%s", options->name[i], options->name[i + 1] ? ", " : "]\n" );
-    debugl("    client_nb: %d\n"
+        printf("%s%s", options->name[i], options->name[i + 1] ? ", " : "]\n" );
+    printf("    client_nb: %d\n"
             "    freq: %d\n"
             "}\n", options->client_nb, options->freq);
 }
