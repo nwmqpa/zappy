fbf723e1b6f5779917a19a37b3aa137c9770db63
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-05 16:43:27 +0200

    :sparkles: :zap: Adding look command for client.

--- server/sources/client/look.c
+++ server/sources/client/look.c
@@ -5,13 +5,63 @@
 ** Look around command file.
 */
 
+#include "map.h"
 #include "client_commands.h"
 
-char *get_vision_content(server_t *server, unsigned int level,
+static char *get_view_level(int level, pos_t pos, map_t *map)
+{
+    int sub_level = level - 1;
+    int begin = sub_level * -1;
+    const char **stock = malloc(sizeof(char *) * sub_level);
+    int total_len = 0;
+    char *line;
+    int idx = 0;
+    char *ret;
+
+    while (begin <= sub_level) {
+        line = tile_to_string(get_tile_map(map, pos.x - begin, pos.y + level - 1));
+        total_len += strlen(line) + 1;
+        stock[idx++] = line;
+        begin++;
+    }
+    if (level == 1)
+        return line;
+    ret = calloc(sizeof(char), total_len);
+    join_str(ret, stock, ',', idx);
+    return ret;
+}
+
+// Get vision use the player level, direction and position to give him what he
+// can see on the tile in front of him.
+// Example for north position and level 3:
+//
+//                   [...]
+// [x - 1,y + 2][x + 0,y + 2][x + 1,y + 2]
+//               [x + 0,y + 1]
+//                    [P]
+//
+// where P is player.
+// x is player x position.
+// y is player y position.
+// /!\ The world is spherical.
+//
+static char *get_vision_content(server_t *server, unsigned int level,
         unsigned int direction, pos_t position)
 {
-    // TODO: Implement.
-    return "[tile1, tile2, tile3]";
+    char *buffer = NULL;
+    char *tmp = NULL;
+    char *new = NULL;
+
+    for (unsigned int i = 1; i <= level; ++i) {
+        new = get_view_level(i, position, server->map);
+        if (tmp != NULL)
+            asprintf(&buffer, "%s,%s", tmp, new);
+        else
+            buffer = new;
+        tmp = buffer;
+    }
+    asprintf(&buffer, "[%s]", tmp);
+    return buffer;
 }
 
 char *look(client_t *client, server_t *server)
