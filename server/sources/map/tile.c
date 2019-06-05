76665b77c6d1a93e20137915b0a34d05a4fb8d18
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-05 16:42:35 +0200

    :zap: Adding function to transform a tile into a string `[linemate:12 food:10 player:2]`.

--- server/sources/map/tile.c
+++ server/sources/map/tile.c
@@ -5,9 +5,20 @@
 ** Tile functions.
 */
 
+#include <string.h>
 #include "map.h"
 
 static const unsigned int MAX_RAND_TILE = 20;
+
+static const char *RESOURCES_NAME[] = {
+    "linemate",
+    "deraumere",
+    "sibur",
+    "mendiane",
+    "phiras",
+    "thystame",
+    "food"
+};
 
 void set_tile(tile_t *new)
 {
@@ -27,9 +38,35 @@ void add_player(tile_t *tile, int id)
     for (unsigned int i = 0; i < tile->nb_player; ++i)
         new[i] = tile->player_ids[i];
     new[tile->nb_player] = id;
-    tile->nb_player += 1;
+    tile->nb_player++;
     free(tile->player_ids);
     tile->player_ids = new;
+}
+
+char *tile_to_string(const tile_t *tile)
+{
+    char *res = NULL;
+    char *stock[9] = {NULL};
+    int value;
+    size_t total_len = 0;
+    int nb_elem = 0;
+
+    for (int i = 0; i < INVENTORY_SIZE; ++i) {
+        value = tile->inventory.inv_arr[i];
+        if (value > 0) {
+            asprintf(&stock[i], "%s:%d", RESOURCES_NAME[i], value);
+            total_len += strlen(stock[i]) + 1;
+            nb_elem++;
+        }
+    }
+    if (tile->nb_player > 0) {
+        asprintf(&stock[8], "player:%d", tile->nb_player);
+        total_len += strlen(stock[8]) + 1;
+        nb_elem++;
+    }
+    res = calloc(sizeof(char), total_len);
+    join_str(res, stock, ' ', nb_elem);
+    return res;
 }
 
 void remove_player(tile_t *tile, int id)
@@ -37,6 +74,7 @@ void remove_player(tile_t *tile, int id)
     for (unsigned int i = 0; i < tile->nb_player; ++i) {
         if (tile->player_ids[i] == id) {
             tile->player_ids[i] = -1;
+            tile->nb_player--;
             return;
         }
     }
