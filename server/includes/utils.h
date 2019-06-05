f914f1b093a53e880e5a803cfadc94635b34fa44
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-05 16:41:51 +0200

    :zap: Adding header for utils.c file.

---
+++ server/includes/utils.h
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2019
+** utils.h
+** File description:
+** Utils header.
+*/
+
+#pragma once
+
+#include "string.h"
+
+int join_str(char *insert, const char **to_join, char join, size_t size);
