7239b64871cf9674baa350adf31dc2911ab4bddd
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-14 16:50:57 +0200

    :sparkles: Placing players randomly in the map at connection.

--- server/sources/server/client_handler.c
+++ server/sources/server/client_handler.c
@@ -5,6 +5,7 @@
 ** Client handler function callback.
 */
 
+#include "map.h"
 #include "logger.h"
 #include "generic_list.h"
 #include "client_commands.h"
@@ -25,6 +26,9 @@ void handle_protocol(client_t *client, server_t *server)
     dprintf(client->id, "WELCOME\n");
     read(client->id, &team_name, 100);
     free_space = add_client_to_team(server, client, team_name);
+    if (free_space > 0)
+        add_player(get_random_tile(server->map, server->width, server->height),
+                client->id);
     dprintf(client->id, "%d\n", free_space - 1);
 }
 
