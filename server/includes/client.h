61c2dd364a48b0113c3ac054e0f62f7f28116435
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-15 16:58:31 +0200

    :zap: Changing client_t::to_send to client_t::to_exec.

--- server/includes/client.h
+++ server/includes/client.h
@@ -62,7 +62,7 @@ typedef struct pos_s {
 ** -> direction: Where the player is facing.
 ** -> position: [x, y] position defining on which tile the player is positioned.
 ** -> cooldown: Time that left to before launching the new incante.
-** -> to_send: When cooldown is over this will be send to the player.
+** -> to_send: When cooldown is over this will be executed.
 */
 typedef struct client_s {
     int id;
@@ -72,7 +72,7 @@ typedef struct client_s {
     pos_t position;
     inventory_t inventory;
     int cooldown;
-    char *to_send;
+    char *to_exec;
 } client_t;
 
 // Utility function.
