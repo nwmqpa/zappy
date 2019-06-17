64a587aeec88122f608e30ba870de8250745d0c6
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-17 15:13:11 +0200

    :books: Adding documentation on some protocol function.

--- server/includes/protocols_graphic.h
+++ server/includes/protocols_graphic.h
@@ -11,6 +11,10 @@
 #include "server.h"
 #include "logger.h"
 
+/// Register all handlers onto the server.
+void register_all_handlers(server_t *server);
+
+/// Handle a graphic packet.
 int handle_packet(int socket, server_t *server);
 
 int map_size(const void *data);
