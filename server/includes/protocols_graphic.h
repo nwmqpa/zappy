c952a54a29d462e04d2a911285699094454101c7
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-17 14:52:04 +0200

    :sparkles: Adding header for function handlers.

---
+++ server/includes/protocols_graphic.h
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2019
+** protocols_graphic.h
+** File description:
+** Protocol functions.
+*/
+
+#pragma once
+
+#include "protocols.h"
+#include "server.h"
+#include "logger.h"
+
+int handle_packet(int socket, server_t *server);
+
+int map_size(const void *data);
+int map_content(const void *data);
+int tile_content(const void *data);
+
+int teams_name(const void *data);
+
+int player_pos(const void *data);
