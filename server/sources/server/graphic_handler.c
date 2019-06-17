0b71a24699de4c902a41d9696596ba323b757a40
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-17 14:52:26 +0200

    :sparkles: Adding graph packet handling to dispatching.

--- server/sources/server/graphic_handler.c
+++ server/sources/server/graphic_handler.c
@@ -6,21 +6,25 @@
 */
 
 #include "logger.h"
+#include "protocols_graphic.h"
 
 int on_connect_graphic(int socket, void *data)
 {
-    debugl("Graphic connect handler.\n");
+    debugl("New graphic client connected.\n");
     return 0;
 }
 
 int on_delete_graphic(int socket, void *data)
 {
-    debugl("Graphic delete handler.\n");
+    debugl("A graphic client just disconnect.\n");
     return 0;
 }
 
 int on_active_graphic(int socket, void *data)
 {
-    debugl("Graphic active handler.\n");
+    server_t *server = (server_t *) data;
+
+    debugl("Graphic client sent a message.\n");
+    handle_packet(socket, server);
     return 0;
 }
