e14f31b8bbea19e430c84a764aa79d25a9a2f875
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-19 10:37:17 +0200

    :sparkles: Working template class `DataHandler<T>`!

--- graphic_client/includes/DataHandler.hpp
+++ graphic_client/includes/DataHandler.hpp
@@ -13,7 +13,7 @@
 template <typename T>
 class DataHandler {
 
-typedef void (*handler) (T &data);
+typedef bool (*handler) (int sock, T &data);
 
 public:
     /// DataHandler constructor that need a function
@@ -22,26 +22,46 @@ public:
         , _fd(epoll_create1(0))
         , _handlerFunc(func)
         , _eventCount(0)
-        , _events{0, 0, 0, 0, 0}
+        , _events{}
     {
         if (_fd == -1) {
-            std::cerr << "Error cannot create epoll." << std::endl;
+            std::cerr << "Error cannot create epoll socket." << std::endl;
+            exit(84);
+        }
+        struct epoll_event ev = {};
+
+        ev.events = EPOLLIN;
+        ev.data.fd = _sock;
+        if (epoll_ctl(_fd, EPOLL_CTL_ADD, _sock, &ev)) {
+            std::cerr << "Error cannot add epoll sock." << std::endl;
             exit(84);
         }
     }
 
     /// Handler of the data.
-    void handle(T &data) noexcept {
+    bool handle(T &data) noexcept {
         _eventCount = epoll_wait(_fd, _events, 5, 1);
 
         for (int i = 0; i < _eventCount; ++i) {
-            if (_events[i].data.fd == _sock) {
-                _handlerFunc(data);
+            if (_handlerFunc(_sock, data) == false) {
+                cleanupSocket();
+                return false;
             }
         }
+        return true;
     }
 
 private:
+    void cleanupSocket() noexcept {
+        struct epoll_event event = {};
+
+        event.events = EPOLLIN;
+        event.data.fd = _sock;
+        if (epoll_ctl(_fd, EPOLL_CTL_DEL, _sock, &event))
+            std::cerr << "Connection with the server closed.\n" << std::endl;
+        close(_sock);
+    }
+
     int _sock;
     int _fd;
     handler _handlerFunc;
