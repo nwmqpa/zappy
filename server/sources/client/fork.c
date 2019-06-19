67916252a3eac8817b40673c90473a1692847b34
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-19 12:32:21 +0200

    :sparkles: :hammer: Rework fork to add eggs to the game.

--- server/sources/client/fork.c
+++ server/sources/client/fork.c
@@ -7,17 +7,21 @@
 
 #include "client_commands.h"
 #include "logger.h"
+#include "egg.h"
 
 // TODO: Adding fork.
 char *fork_client(client_t *client, server_t *server)
 {
     team_t *team = get_client_team(client, server);
-    client_t *new = client_create(-2);
+    client_t *tmp = client_create(-2);
+    egg_t *new;
 
-    if (add_client_to_team(server, new, team->name) == 0) {
+    if (add_client_to_team(server, tmp, team->name) == 0) {
         errorl("Cannot fork no more space in team %s.\n", team->name);
         return strdup("ko");
     }
+    new = egg_create(client->position.x, client->position.y);
+    append_list(server->eggs, new);
     return strdup("ok");
 }
 
