03374be63921b62f3d71d874027c2dfe778326f7
Alexandre Fourcat <alexandre.fourcat@epitech.eu>
2019-06-21 23:53:16 +0200

    :hammer: :bug: Fixing memory error in join_str.

--- server/sources/utils/utils.c
+++ server/sources/utils/utils.c
@@ -9,25 +9,10 @@
 #include "client.h"
 #include "egg.h"
 
-int join_str(char *insert, const char **to_join, char join, size_t size)
+int join_str(char *insert, const char **to_join, char *join, size_t size)
 {
-    int offset = 0;
-    int l_len = 0;
-    size_t i = 0;
-    int y = -1;
-
-    for (i = 0; i < size; ++i) {
-        if (to_join[++y] == NULL) {
-            i--;
-            continue;
-        }
-        l_len = strlen(to_join[y]);
-        strncpy(insert + offset, to_join[y], l_len);
-        offset += l_len + 1;
-        if (i + 1 != size)
-            insert[offset - 1] = join;
-    }
-    insert[offset] = 0;
+    for (size_t i = 0; i < size; ++i)
+        sprintf(insert, "%s%s%s", insert, (i == 0) ? "" : join, to_join[i]);
     return 0;
 }
 
