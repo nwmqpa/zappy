98ec481948ee743a987affdea195e86b4040a583
Thomas Nicollet <thomas.nicollet@epitech.eu>
2019-06-23 18:31:29 +0200

    :bug: Corrected rendering.

--- graphic_client/sources/Isometry.cpp
+++ graphic_client/sources/Isometry.cpp
@@ -9,17 +9,17 @@
 
 float Window::Isometry::getIsoX(int cartX, int cartY, float surfaceWidth, float surfaceHeight, float screenWidth, float screenHeight, Camera camera)
 {
-    float ret = ((cartX * surfaceWidth) - (cartY * surfaceWidth)) / 2;
+    float ret = ((cartY * surfaceWidth) - (cartX * surfaceWidth)) / 2;
     ret += (screenWidth / 2) - (surfaceWidth / 2);
     ret /= camera.scale.x;
     ret += camera.center.x;
     return ret;
 }
 
-float Window::Isometry::getIsoY(int cartX, int cartY, float surfaceWidth, float surfaceHeight, float screenWidth, float screenHeight, Camera camera)
+float Window::Isometry::getIsoY(int cartX, int cartY, float surfaceWidth, float surfaceHeight, float screenWidth, float screenHeight, float mapHeight, Camera camera)
 {
-    float ret = ((cartX * surfaceHeight) + (cartY * surfaceHeight)) / 4;
-    ret += (screenHeight / 2) - (surfaceHeight / 2) * (screenHeight / 2);
+    float ret = ((cartY * surfaceHeight) + (cartX * surfaceHeight)) / 4;
+    ret += (screenHeight / 2) - (surfaceHeight / 2) * (mapHeight / 2);
     ret /= camera.scale.y;
     ret += camera.center.y;
     return ret;
